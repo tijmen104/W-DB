c51c0834aed430586041974e79f7aa8b69a2a545
Daan Alkemade <28051445+DaanAlk@users.noreply.github.com>
2018-11-30 17:40:27 +0100

    add ships generation to javascript

--- myapp/public/javascripts/interactions.js
+++ myapp/public/javascripts/interactions.js
@@ -4,5 +4,9 @@ function GameState () {
 
 (function setUp () {
     generateBoards();
+    var ships = new Ships();
+    ships.addShip(new Ship(3));
+    ships.addShip(new Ship(2));
+    generateShips(ships);
 
 })(); //execute immediately
--- myapp/public/javascripts/ships.js
+++ myapp/public/javascripts/ships.js
@@ -1,3 +1,26 @@
 function Ships() {
-    
+    this.array = [];
+}
+Ships.prototype.addShip = function(ship) {
+    this.array.push(ship);
+}
+
+function Ship(length) {
+    this.length = length;
+}
+Ship.prototype.setCoordinates = function (coordinates) {
+    this.coordinates = coordinates;
+}
+
+var generateShips = function(ships) {
+    var col = "";
+    var space = 1;
+    for (i = 0; i < ships.array.length; i++) {
+        ship = ships.array[i];
+        // TODO: it's not good how the sizes are hardcoded here
+        command = "<img draggable=\"true\" border=\"2\" width=\"" + ship.length*60 + "\" height=\"60\"";
+        col += "<tr data-pos='"+space+"'><td>"+ command +"</td></tr>"; 
+        space++; 
+    }
+    $("#shipTable").append(col);
 }
