77ed678ef8417b6ce4356652faae92f2196ac59a
TMU <tijmen104@gmail.com>
2018-12-06 16:05:28 +0100

    add websocket

--- myapp/app.js
+++ myapp/app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var http = require("http");
+var websocket = require("ws");
 
 var port = process.argv[2];
 var app = express();
@@ -11,4 +12,22 @@ app.use(express.static(__dirname + "/public"));
 app.get("/game",indexRouter);
 app.get("/splash",indexRouter);
 
-http.createServer(app).listen(port);
+var server = http.createServer(app);
+const wss = new websocket.Server({ server });
+
+wss.on("connection", function(ws) {
+
+    //let's slow down the server response time a bit to make the change visible on the client side
+    setTimeout(function() {
+        console.log("Connection state: "+ ws.readyState);
+        ws.send("Thanks for the message. --Your server.");
+        ws.close();
+        console.log("Connection state: "+ ws.readyState);
+    }, 2000);
+    
+    ws.on("message", function incoming(message) {
+        console.log("[LOG] " + message);
+    });
+});
+
+server.listen(port);
---
+++ myapp/public/javascripts/config.js
@@ -0,0 +1,10 @@
+(function setup(){
+var socket = new WebSocket("ws://localhost:3000");
+socket.onmessage = function(event){
+    document.getElementById("hello").innerHTML = event.data;
+}
+socket.onopen = function(){
+    socket.send("Hello from the client!");
+    document.getElementById("hello").innerHTML = "Sending a first message to the server ...";
+};
+})();
