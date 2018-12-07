d6d18b926e319e38d357bc012eccd1b7452a0f39
Daan Alkemade <28051445+DaanAlk@users.noreply.github.com>
2018-12-07 14:15:36 +0100

    save ship coordinates upon drop

--- myapp/public/javascripts/interactions.js
+++ myapp/public/javascripts/interactions.js
@@ -17,6 +17,8 @@ function GameState (session_id, ships) {
     }
 
     this.updateGame = function(clickedLetter) {
+        console.log(ships);
+
         var button = document.getElementById(clickedLetter)
 
         var row = $(button.parentElement).parent().index();
@@ -72,16 +74,19 @@ function ButtonsProcessor(gs){
 (function setUp () {
     socketSetup();
     generateBoards();
-    var ships = new Ships();
-    var ship1 = new Ship(3);
-    ship1.setCoordinates([new Coordinate(1,1), new Coordinate(1,2)]); //TODO: remove hardcoded ship coordinates
-    var ship2 = new Ship(2);
-    ship2.setCoordinates([new Coordinate(2,1), new Coordinate(2,2)]); //TODO: remove hardcoded ship coordinates
+    ships = new Ships(); //global
+    var ship1 = new Ship(3, "ship1");
+    var ship2 = new Ship(2, "ship2");
+
+    // ship1.setCoordinates([new Coordinate(1,1), new Coordinate(1,2)]); //TODO: remove hardcoded ship coordinates
+    // ship2.setCoordinates([new Coordinate(2,1), new Coordinate(2,2)]); //TODO: remove hardcoded ship coordinates
     ships.addShip(ship1);
     ships.addShip(ship2);
 
     
     generateShips(ships);
+
+
 
     var gs = new GameState(1, ships);
 
--- myapp/public/javascripts/ships.js
+++ myapp/public/javascripts/ships.js
@@ -5,9 +5,10 @@ Ships.prototype.addShip = function(ship) {
     this.array.push(ship);
 }
 
-function Ship(length) {
+function Ship(length, id) {
     this.length = length;
     this.sunk = false;
+    this.id = id;
 }
 Ship.prototype.setCoordinates = function (coordinates) {
     this.coordinates = coordinates;
@@ -17,6 +18,9 @@ Ship.prototype.getCoordinates = function () {
 }
 Ship.prototype.sink = function () {
     this.sunk = true;
+}
+Ship.prototype.getId = function () {
+    return this.id;
 }
 
 function Coordinate (x, y) {
@@ -44,24 +48,14 @@ function allowDrop(ev) {
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
     ev.dataTransfer.setData("sourcePlaceHolderId", ev.target.parentElement.firstChild.id)
-    // ev.target.parentElement.
-    // var placeHolder = ev.target.parentElement.firstChild
-
-
-
-
-}
-
-var validateCoordinates = function (row, column) {
-    return true; //TODO
 }
 
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     
-    var row = $(ev.target).parent().index();
-    var column = $(ev.target).parent().parent().index();
+    var row = $(ev.target).parent().parent().index();
+    var column = $(ev.target).parent().index();
     console.log("Dropped front of ship on coordinates [" + row + "," + column + "]");
     if (validateCoordinates(row, column)) {
         ev.target.parentElement.appendChild(document.getElementById(data));
@@ -76,10 +70,30 @@ function drop(ev) {
             placeHolder.style.height = "100%";
             console.log("reset size of placeholder");
         }
+
+        
+        var updateCoordinates = function(ship) {
+            if (ship.id == data) {
+                var coordinates = [];
+                for (i =0; i < ship.length; i++) {
+                    coordinates.push(new Coordinate(row, column+i));
+                }
+                ship.setCoordinates(coordinates);
+                console.log(ship);
+            }
+        }
+        ships.array.forEach(updateCoordinates);
+        
     }
     
     
 }
+
+var validateCoordinates = function (row, column) {
+    return true; //TODO
+}
+
+
 
 var generateShips = function(ships) {
     var col = "";
@@ -87,7 +101,7 @@ var generateShips = function(ships) {
     for (i = 0; i < ships.array.length; i++) {
         ship = ships.array[i];
         // TODO: it's not good how the sizes are hardcoded here
-        command = "<img draggable=\"true\" ondragstart=\"drag(event)\" border=\"2\" width=\"" + ship.length*60 + "px\" height=\"50px\" src=\"images/LogoBBB.png\" class=ship id=ship"+ ship.length*60;
+        command = "<img draggable=\"true\" ondragstart=\"drag(event)\" border=\"2\" width=\"" + ship.length*60 + "px\" height=\"50px\" src=\"images/LogoBBB.png\" class=ship id=" + ship.getId() + ">";
         col += "<td>"+ command +"</td>"; 
         space++; 
     }
