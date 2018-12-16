d3ffd2d2576fd5677ddda4ed594f1d5153c046cc
TMU <tijmen104@gmail.com>
2018-12-16 11:40:17 +0100

    remake message file

--- myapp/public/javascripts/messages.js
+++ myapp/public/javascripts/messages.js
@@ -1,48 +1,5 @@
 (function(exports){
     /*
-    * server to client: your turn to shoot
-    */
-   exports.T_SHOOT = "Your-turn";
-   exports.O_SHOOT = {
-       type: exports.T_SHOOT,
-       data: null
-    };
-
-    /*
-    * client to server: I shot
-    */
-   exports.T_SHOT = "I shot";
-   exports.O_SHOT = {
-       type: exports.T_SHOT,
-       data: null
-    };
-   
-
-   /*
-   * Server to client: Game started
-   */
-  exports.T_GAME_STARTED = {
-      type: "GAME-STARTED"
-  };
-  exports.O_GAME_STARTED = JSON.stringify(exports.T_GAME_STARTED);
-  /*
-  * Client to server: move made
-  */
- exports.T_MOVE_MADE ="MOVE-MADE";
- exports.O_MOVE_MADE ={
-    type: exports.T_MOVE_MADE,
-    data:null
- };
- /**
-  * Client to server: game ended;
-  */
- exports.T_GAME_ENDED = "GAME-ENDED";
- exports.O_GAME_ENDED = {
-    type: exports.T_GAME_ENDED,
-    data:null
- };
-
- /*
      * Server to client: set as player A 
      */
     exports.T_PLAYER_TYPE = "PLAYER-TYPE";
@@ -62,18 +19,66 @@
     exports.S_PLAYER_B = JSON.stringify(exports.O_PLAYER_B);
 
     /**
-     * Client to server: set board
+     * Server to client: set ships
+     */
+    exports.T_PLAYERS_CONNECTED={
+        type:"PLAYERS-CONNECTED"
+    };
+    exports.S_PLAYERS_CONNECTED = JSON.stringify(exports.T_PLAYERS_CONNECTED);
+
+    /**
+     * Client to server: I set board
      */
     exports.T_SHIPS = "SHIPS";
     exports.O_SHIPS_SET ={
         type: exports.T_SHIPS,
         data: null
     };
-
     /**
      * Server to client: board set 
      */
     exports.S_SHIPS_SET = JSON.stringify(exports.O_SHIPS_SET);
+    /*
+   * Server to client: Game started
+   */
+    exports.T_GAME_STARTED = {
+        type: "GAME-STARTED"
+    };
 
+    exports.O_GAME_STARTED = JSON.stringify(exports.T_GAME_STARTED);
+    /*
+    * server to client: your turn to shoot
+    */
+   exports.T_SHOOT = "Your-turn";
+   exports.O_SHOOT = {
+       type: exports.T_SHOOT,
+       data: null
+    };
+
+    /*
+    * client to server: I shot
+    */
+   exports.T_SHOT = "I shot";
+   exports.O_SHOT = {
+       type: exports.T_SHOT,
+       data: null
+    };
+   
+  /*
+  * Client to server: move made
+  */
+ exports.T_MOVE_MADE ="MOVE-MADE";
+ exports.O_MOVE_MADE ={
+    type: exports.T_MOVE_MADE,
+    data:null
+ };
+ /**
+  * Client to server: game ended;
+  */
+ exports.T_GAME_ENDED = "GAME-ENDED";
+ exports.O_GAME_ENDED = {
+    type: exports.T_GAME_ENDED,
+    data: null
+ };
 
 }(typeof exports === "undefined" ? this.Messages = {} : exports)); 
